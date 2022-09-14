de4b1bd83b42f0936f849ba0c334e73efa0fd77a
ClarkDylan <clarkdylan00@gmail.com>
2022-09-14 18:23:20 -0500

    adds weather info to search using previous search

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const $state = $('#inputState');
 const $searchBtn = $("#searchButton");
 
 $searchBtn.click(function () {
-  $.get(`http://api.openweathermap.org/geo/1.0/direct?q=${$city.val()},${$state.val()},${country}&appid=${apiKey}`, (data) => {
+  // used to get lat and long data
+  $.get(`http://api.openweathermap.org/geo/1.0/direct?q=${$city.val()},${$state.val()},${country}&appid=${apiKey}`, (geo) => {
 
-    console.log(data);
+    console.log(geo);
+    let geoInfo = geo[0]
+    let lat = geoInfo.lat
+    let long = geoInfo.lon
+    function updateWidget() {
+      // inputs lat and long to get temp & weather condition
+      $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`, (weatherInfo) => {
+        console.log(weatherInfo);
+
+      });
+    }
+    updateWidget();
   });
 })
