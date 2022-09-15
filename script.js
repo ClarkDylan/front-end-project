ad9ec96e7680f14b2ac5c4e5a48890226012f323
ClarkDylan <clarkdylan00@gmail.com>
2022-09-15 15:17:05 -0500

    adds start of forecast feature, provides day img

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const apiKey = '5e23915a02e4de0ff91d9759230991bb';
 const country = 'us';
 const $userCity = $('#inputCity');
 const $userState = $('#inputState');
-const $searchBtn = $("#searchButton");
+const $searchBtn = $('#searchButton');
+const $selector = $('#displaySelector')
 
 $searchBtn.click(function () {
   //  gets lat and long data from geo API
@@ -16,35 +17,63 @@ $searchBtn.click(function () {
       // inputs lat and long to get temp & weather condition from weather api
       $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`, (weatherInfo) => {
 
-        //replaces placeholder div with blank div
-        $('#weatherResults').replaceWith('<div id="weatherResults"></div>');
+        if ($selector.val() == 'today') {
 
-        // converts temp data to celcius for late use (dependent on user choice)
-        let fToC = ((weatherInfo.main['temp'] - 32) * 5) / 9
+          //replaces placeholder div with blank div
+          $('#weatherResults').replaceWith('<div id="weatherResults"></div>');
 
-        // updates image based on weather condition
-        let $img = $(`<img class="weatherIcon" src="/assets/${weatherInfo.weather[0].main}.png">`)
-        $img.appendTo('#weatherResults');
+          // converts temp data to celcius for late use (dependent on user choice)
+          let farToCel = ((weatherInfo.main['temp'] - 32) * 5) / 9
 
-        let $fBtn = $('#fDegrees');
-        let $cBtn = $('#cDegrees');
+          // updates image based on weather condition
+          let $img = $(`<img class="weatherIcon" src="/assets/${weatherInfo.weather[0].main}.png">`)
+          $img.appendTo('#weatherResults');
 
-        // checks value of radio buttons allowing user to choose temp unit
-        if ($cBtn[0].checked) {
-          let $temp = $(`<h1 class="temp">${Math.floor(fToC)}°C</h1>`);
-          $temp.appendTo('#weatherResults');
-        } else {
-          let $temp = $(`<h1 class="temp">${Math.floor(weatherInfo.main['temp'])}°F</h1>`);
-          $temp.appendTo('#weatherResults');
+          let $fBtn = $('#fDegrees');
+          let $cBtn = $('#cDegrees');
+
+          // checks value of radio buttons allowing user to choose temp unit
+          if ($cBtn[0].checked) {
+            let $temp = $(`<h1 class="temp">${Math.floor(farToCel)}°C</h1>`);
+            $temp.appendTo('#weatherResults');
+          } else {
+            let $temp = $(`<h1 class="temp">${Math.floor(weatherInfo.main['temp'])}°F</h1>`);
+            $temp.appendTo('#weatherResults');
+          }
+
+          // updates condition of day
+          let $condition = $(`<h1 class="condition">${weatherInfo.weather[0].description.toUpperCase()}</h1>`);
+          $condition.appendTo('#weatherResults');
+
+          // displays city of weather condition
+          let $city = $(`<h2 class="city">${weatherInfo.name.toUpperCase()}</h2>`);
+          $city.appendTo('#weatherResults');
+
+        } else if ($selector.val() == 'forecast') {
+
+          $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`, (forecast) => {
+            console.log(forecast);
+
+            //replaces placeholder div with blank div
+            $('#weatherResults').replaceWith('<div id="weatherResults"></div>');
+
+            let $dayOneImg = $(`<img class="forecastIcon" src="/assets/${forecast.list[0].weather[0].main}.png">`)
+            $dayOneImg.appendTo('#weatherResults');
+
+            let $dayTwoImg = $(`<img class="forecastIcon" src="/assets/${forecast.list[5].weather[0].main}.png">`)
+            $dayTwoImg.appendTo('#weatherResults');
+
+            let $dayThreeImg = $(`<img class="forecastIcon" src="/assets/${forecast.list[13].weather[0].main}.png">`)
+            $dayThreeImg.appendTo('#weatherResults');
+
+            let $dayFourImg = $(`<img class="forecastIcon" src="/assets/${forecast.list[21].weather[0].main}.png">`)
+            $dayFourImg.appendTo('#weatherResults');
+
+            let $dayFiveImg = $(`<img class="forecastIcon" src="/assets/${forecast.list[29].weather[0].main}.png">`)
+            $dayFiveImg.appendTo('#weatherResults');
+
+          });
         }
-
-        // updates condition of day
-        let $condition = $(`<h1 class="condition">${weatherInfo.weather[0].description.toUpperCase()}</h1>`);
-        $condition.appendTo('#weatherResults');
-
-        // displays city of weather condition
-        let $city = $(`<h2 class="city">${weatherInfo.name.toUpperCase()}</h2>`);
-        $city.appendTo('#weatherResults');
       });
     }
     updateWidget();
